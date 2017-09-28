c5ea34b99fc603fa66eb39aa163f486a64835aff
Jon Moguilevsky <jmoguilevsky@gmail.com>
2017-09-28 20:58:45 -0300

    added naive suffix array

--- naive/naive.py
+++ naive/naive.py
@@ -12,10 +12,33 @@ def naiveFind(pattern, text):
     return False
 
 
+def naivelyMakeSuffixArray(source):
+    """
+    A naive, slow suffix-array construction algorithm.
+    """
+
+    # Construct a list of suffixes of the source string.
+    suffixes = []
+    for offset in range(len(source) + 1):
+        suffixes.append(source[offset:])
+
+    # Sort the suffixes
+    suffixes.sort()
+
+    # Calculate the start offset of each suffix, storing them in
+    # sorted order into the suffix array.
+    suffixArray = []
+    for suffix in suffixes:
+        offset = len(source) - len(suffix)
+        suffixArray.append(offset)
+
+    return suffixArray
+
 def test():
     print(naiveFind('hola', 'holaComoTeVa'))
     print(naiveFind('hla', 'holaComoTeVa'))
     print(naiveFind('hola', 'ComoholaTeVa'))
+    print(naivelyMakeSuffixArray('banana'))
 
 if __name__ == '__main__':
     test()
